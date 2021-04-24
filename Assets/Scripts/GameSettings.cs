fa22d68228f216878013f5b8742fe6d0fb9c77ee
Russell <74921709+RNairn01@users.noreply.github.com>
2021-04-24 16:46:15 +0100

    Implemented user input

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Difficulty
+{
+   Easy,
+   Normal,
+   Hard
+}
+public class GameSettings : MonoBehaviour
+{
+   public static Difficulty gameDifficulty = Difficulty.Normal;
+   public static int timerLength;
+   public static int lives;
+   public static float scoreMultiplier;
+
+   public static void ImplementGameSettings()
+   {
+      switch (gameDifficulty)
+      {
+         case Difficulty.Easy:
+            timerLength = 15;
+            lives = 5;
+            scoreMultiplier = 1;
+            break;
+         case Difficulty.Normal:
+            timerLength = 10;
+            lives = 3;
+            scoreMultiplier = 1.5f;
+            break;
+         case Difficulty.Hard:
+            timerLength = 5;
+            lives = 3;
+            scoreMultiplier = 2;
+            break;
+      }
+   }
+}
---
+++ Assets/Scripts/TakeInput.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  UnityEngine.UI;
+using TMPro;
+using System.Text.RegularExpressions;
+
+public class TakeInput : MonoBehaviour
+
+{
+    public TextMeshProUGUI textMesh;
+    private Regex rx;
+    private List<string> currentInput;
+    private LTDescr blinkCall;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rx = new Regex("[A-Za-z]");
+        currentInput = new List<string>();
+        currentInput.Add("|");
+    }
+
+    private void Awake()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageInput();
+        if (willBlink) CursorBlink();
+        
+        
+        
+        textMesh.text = string.Join("", currentInput); 
+    }
+    
+    private bool canBackspace = true;
+    private bool willBlink = true;
+    private bool canBlink = true;
+    private void ManageInput()
+    {
+
+        string charToTest = Input.inputString;
+        if (rx.IsMatch(charToTest))
+        {
+            currentInput[currentInput.Count - 1] = charToTest;
+            currentInput.Add("|");
+            /*
+            willBlink = false;
+            try
+            {
+                LeanTween.cancel(blinkCall.uniqueId);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        
+
+            blinkCall = LeanTween.delayedCall(0.5f, () => willBlink = true);
+            */
+            
+        }
+
+        if (Input.GetKey(KeyCode.Backspace) && canBackspace && currentInput.Count > 1)
+        {
+            canBackspace = false;
+            currentInput.RemoveAt(currentInput.Count - 2);
+            LeanTween.delayedCall(0.08f, () => canBackspace = true);
+
+        };
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            string submittedString = string.Join("",currentInput.GetRange(0, currentInput.Count-1));
+            currentInput.RemoveRange(0, currentInput.Count - 1);
+            Debug.Log(submittedString);
+            
+            //TODO: Implement submission checking against dictionary
+        }
+    }
+
+    private void CursorBlink()
+    {
+        if (canBlink)
+        {
+            currentInput[currentInput.Count - 1] = " ";
+            canBlink = false;
+            LeanTween.delayedCall(0.5f, () =>
+            {
+                currentInput[currentInput.Count - 1] = "|";
+                LeanTween.delayedCall(0.5f, () => canBlink = true);
+            });
+        }
+
+    }
+}
